54c36b5b83e90699d7039118aa8fea51c8341f38
Napon Taratan <napon.taratan@gmail.com>
2014-05-15 12:35:31 -0700

    -Created the app -Added app permissions -Added misc files to .gitignore -Added basic model classes

---
+++ src/model/LatLon.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ * Simple class to represent a Location
+ * @author napontaratan
+ */
+public class LatLon {
+	
+	int latitude;
+	int longitude;
+	
+	public LatLon(int lat, int lon){
+		latitude = lat;
+		longitude = lon;
+	}
+	
+	public int getLatitude(){
+		return this.latitude;
+	}
+	
+	public int getLongitude(){
+		return this.longitude;
+	}
+}
---
+++ src/model/WiFiPoint.java
@@ -0,0 +1,20 @@
+package model;
+
+/**
+ * An object to be pushed to the server
+ * - contains information about a WiFi Location
+ * @author napontaratan
+ */
+public class WiFiPoint {
+	
+	String name;
+	int signalStrength;
+	LatLon location;
+	
+	public WiFiPoint(String name, int signal, int lat, int lon){
+		this.name = name;
+		this.signalStrength = signal;
+		this.location = new LatLon(lat, lon);
+	}
+	
+}
