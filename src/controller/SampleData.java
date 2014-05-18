a4ddefd020ba61fb6febead8866b7b8857616067
Napon Taratan <napon.taratan@gmail.com>
2014-05-17 18:37:27 -0700

    Added sample response in JSON format

--- src/controller/SampleData.java
+++ src/controller/SampleData.java
@@ -2,6 +2,7 @@ package controller;
 
 public class SampleData {
 
-	public static String SAMPLE_XML_RESPONSE = "<wifi_locations><hotspot><name>ubcvisitor</name><signal_strength>2</signal_strength><latitude>49.260605</latitude><longitude>-123.245994</longitude><userID>12345</userID></hotspot><hotspot><name>ubcsecure</name><signal_strength>3</signal_strength><latitude>49.264542</latitude><longitude>-123.245305</longitude><userID>99999</userID></hotspot></wifi_locations>";
+	public static String SAMPLE_XML_RESPONSE  = "<wifi_locations><hotspot><name>ubcvisitor</name><signal_strength>2</signal_strength><latitude>49.260605</latitude><longitude>-123.245994</longitude><userID>12345</userID></hotspot><hotspot><name>ubcsecure</name><signal_strength>3</signal_strength><latitude>49.264542</latitude><longitude>-123.245305</longitude><userID>99999</userID></hotspot></wifi_locations>";
+	public static String SAMPLE_JSON_RESPONSE = "[{\"Name\":\"ubcvisitor\",\"SignalStrength\":\"2\",\"Latitude\":\"49.260605\",\"Longitude\":\"-123.245994\",\"DateDiscovered\":\"2014-05-17\",\"UserID\":\"12345\"},{\"Name\":\"ubcsecure\",\"SignalStrength\":\"3\",\"Latitude\":\"49.264542\",\"Longitude\":\"-123.245305\",\"DateDiscovered\":\"2014-05-15\",\"UserID\":\"99999\"}]";
 	
 }
