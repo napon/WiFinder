59c3a29a2e52e281218ac650ece483ce931ab339
KurtAhn <k_ahn_lem@hotmail.com>
2014-05-19 19:04:14 -0700

    Created WifiConnection
    TODO:
    - Add a field describing whether a connection is secured, etc.

---
+++ src/model/WifiConnection.java
@@ -0,0 +1,89 @@
+package model;
+
+import android.net.wifi.ScanResult;
+
+/**
+ * Simple struct-like object with description about an available wireless connection.
+ *
+ * @author Kurt Ahn
+ */
+public class WifiConnection {
+    /**
+     * Unique ID of the connection.
+     */
+	public final String ssid;
+	
+	/**
+	 * Unique ID of the access point.
+	 */
+	public final String bssid;
+	
+	/**
+	 * Detected signal strength.
+	 */
+	public final int strength;
+	
+	/**
+	 * Location of discovery.
+	 */
+	public final WifiLocation location;
+	
+	/**
+	 * Time of discovery.
+	 */
+	public final long time;
+	
+	/**
+	 * Unique ID given to the application's user.
+	 */
+	public final String clientId;
+	
+	/*
+	 * TODO: We need information about whether the connection is secured, etc.
+	 */
+	
+	/**
+	 * Construct a WifiConnection.
+	 *
+	 * @param scan - Data obtained from scanning for available wifi connections.
+	 *        Contains the SSID, BSSID, signal strength, time recorded, etc.
+	 * @param location - Location of discovery.
+	 * @param clientId - Unique ID given to the application's user.
+	 *
+	 * @author Kurt Ahn
+	 */
+	public WifiConnection(
+			ScanResult scan, WifiLocation location, String clientId) {
+		this.ssid = scan.SSID;
+		this.bssid = scan.BSSID;
+		this.strength = scan.level;
+		this.location = location;
+		//this.time = scan.timestamp; // This requires min API level of 17
+		this.time = 0L; // Temporary
+		this.clientId = clientId;
+	}
+	
+	/**
+	 * @return Information about the connection formatted as:
+	 * <p>
+	 * SSID: {@link #ssid}</br>
+	 * BSSID: {@link #bssid}</br>
+	 * Strength: {@link #strength}</br>
+	 * Location: {@link #WifiLocation.latitude} {@link #WifiLocation.longitude}</br>
+	 * Time: {@link #time}</br>
+	 * Client ID: {@link #clientId} </br>
+	 * </p>
+	 * 
+	 * @author Kurt Ahn
+	 */
+	@Override
+	public String toString() {
+		return
+			"SSID: " + ssid + "\n" +
+			"BSSID: " + bssid + "\n" +
+			"Strength: " + String.valueOf(strength) + "\n" +
+			"Location: " + location.latitude + " "  + location.longitude + "\n" +
+			"Time: " + String.valueOf(time) + "\n" + 
+			"Client ID: " + clientId;
+	}
+}
